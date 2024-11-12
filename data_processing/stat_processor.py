eb3e5fc7c0699be14abfb757d049e4dfde0c0ec1
nickdgas <nickdags123@example.com>
2024-11-12 17:31:16 -0500

    improve visualizations; decompose dataset; narrow focus to indiv player for subset EDA and test

--- data_processing/stat_processor.py
+++ data_processing/stat_processor.py
@@ -2,6 +2,7 @@ import os
 import logging
 import warnings
 import pandas as pd
+import plotly.graph_objects as go
 import plotly.express as px
 import pendulum
 
@@ -11,19 +12,21 @@ logging.basicConfig(format="%(asctime)s - %(levelname)s - %(message)s")
 processor_logger = logging.getLogger("NFL-Processor")
 processor_logger.setLevel(logging.INFO)
 
-class NFLStatsProcessor():
+
+class NFLStatsProcessor:
     """
     A class to process NFL stats, generate visualizations, and save them for later viewing.
-    
+
     Attributes:
         offensive_positions (list): Positions to process (currently only "QB").
         current_year (int): The current year.
         backfill_year (int): The year to start backfilling the stats from.
     """
-    offensive_positions = ["QB"] #, "RB", "WR", "TE"]
+
+    offensive_positions = ["QB"]  # , "RB", "WR", "TE"]
     current_year = pendulum.now().year
     backfill_year = pendulum.now().subtract(years=(current_year - 2021)).year
-    
+
     def __init__(self):
         """
         Initialize list of files to process for offensive positions and years.
@@ -48,10 +51,14 @@ class NFLStatsProcessor():
         for year in year_range:
             for pos in positions:
                 directory = f"stats/{pos.lower()}_stats/{year}"
-                files[year] = sorted(os.listdir(directory), key=lambda x: int(x.split('_')[-1].split('.')[0]))
+                files[year] = sorted(
+                    os.listdir(directory),
+                    key=lambda x: int(x.split("_")[-1].split(".")[0]),
+                )
         return files
 
-    def read_data(self) -> pd.DataFrame:
+    @property
+    def stats(self) -> pd.DataFrame:
         """
         Read and combine all CSV files for the specified years and positions into one large DataFrame.
 
@@ -69,10 +76,34 @@ class NFLStatsProcessor():
                     df = pd.read_csv(file_path)
                     df["POSITION"] = pos
                     df["YEAR"] = year
-                    df["WEEK"] = int(file.split("_")[-1].split('.')[0])
+                    df["WEEK"] = int(file.split("_")[-1].split(".")[0])
                     df_list.append(df)
         stats_df = pd.concat(df_list, ignore_index=True)
         return stats_df
+
+    def get_player_stats(
+        self, player_name: str, year: int = None, position: str = None
+    ) -> pd.DataFrame:
+        """
+        Extracts individual player's statistics based on player name, year, and position.
+
+        Parameters:
+        - player_name (str): Name of the player to filter for.
+        - year (int, optional): Year of the statistics to filter for. Defaults to None (all years).
+        - position (str, optional): Position of the player to filter for. Defaults to None (all positions).
+
+        Returns:
+        pd.DataFrame: A DataFrame containing the statistics of the specified player.
+        """
+        stats_df = self.stats
+        player_stats = stats_df[
+            stats_df["PLAYER"].str.contains(player_name, case=False, na=False)
+        ]
+        if year:
+            player_stats = player_stats[player_stats["YEAR"] == year]
+        if position:
+            player_stats = player_stats[player_stats["POSITION"] == position]
+        return player_stats
 
     @staticmethod
     def save_plot(fig, filename: str) -> None:
@@ -88,93 +119,148 @@ class NFLStatsProcessor():
         fig.write_html(f"visualizations/{filename}")
         processor_logger.info(f"Saved visualization: {filename}")
 
-    def create_visualizations(self, df: pd.DataFrame) -> None:
+    @staticmethod
+    def create_plot(
+        df: pd.DataFrame, plot_type: str, x_col: str, y_col: str, title: str
+    ) -> go.Figure:
         """
-        Generate and save visualizations for various player statistics.
+        Create a plot based on the given parameters.
 
         Parameters:
-        - df (pd.DataFrame): The cleaned DataFrame containing NFL stats.
-        """
-        self.create_fantasy_points_over_time(df)
-        self.create_passing_yards_vs_fantasy_points(df)
-        self.create_rushing_yards_vs_fantasy_points(df)
-        self.create_deep_balls_vs_fantasy_points(df)
-        self.create_passing_pct_vs_fantasy_points(df)
+        - df (pd.DataFrame): Data for plotting.
+        - plot_type (str): Type of plot ('line', 'scatter', 'bar').
+        - x_col (str): Column name to use for the x-axis.
+        - y_col (str): Column name to use for the y-axis.
+        - title (str): Title of the plot.
 
-    def create_fantasy_points_over_time(self, df: pd.DataFrame) -> None:
+        Returns:
+        plotly.graph_objs.Figure: A Plotly figure object.
         """
-        Create a visualization showing Fantasy Points over time (by Week).
-        
-        Parameters:
-        - df (pd.DataFrame): The DataFrame containing NFL stats.
-        """
-        fig = px.line(df, x="WEEK", y="MISC_FPTS", color="PLAYER", 
-                      title="Fantasy Points Over Time (by Week)",
-                      labels={"MISC_FPTS": "Fantasy Points", "WEEK": "Week", "PLAYER": "Player"})
-        fig.update_traces(mode='markers+lines')
-        self.save_plot(fig, "fantasy_points_over_time.html")
+        if plot_type == "line":
+            fig = px.line(df, x=x_col, y=y_col, title=title, color="YEAR", line_shape="linear")
+        elif plot_type == "scatter":
+            fig = px.scatter(df, x=x_col, y=y_col, title=title)
+        elif plot_type == "bar":
+            fig = px.bar(df, x=x_col, y=y_col, title=title, color="YEAR")
+        elif plot_type == "stack_bar":
+            fig = px.bar(df, x=x_col, y=y_col, title=title, color="YEAR", barmode="group")
+        elif plot_type == "hist":
+            fig = px.histogram(df, x=x_col, nbins=30, title=title)
+        fig.update_layout(showlegend=True)
+        return fig
 
-    def create_passing_yards_vs_fantasy_points(self, df: pd.DataFrame) -> None:
+    def generate_and_save_plots(self, player_name: str) -> None:
         """
-        Create a scatter plot of Passing Yards vs Fantasy Points.
-        
-        Parameters:
-        - df (pd.DataFrame): The DataFrame containing NFL stats.
-        """
-        fig = px.scatter(df, x="PASSING_YDS", y="MISC_FPTS", color="PLAYER", 
-                         title="Passing Yards vs Fantasy Points",
-                         labels={"PASSING_YDS": "Passing Yards", "MISC_FPTS": "Fantasy Points", "PLAYER": "Player"})
-        fig.update_traces(marker=dict(size=8))
-        self.save_plot(fig, "passing_yards_vs_fantasy_points.html")
+        Generate and save multiple visualizations for a given player's performance over time.
 
-    def create_rushing_yards_vs_fantasy_points(self, df: pd.DataFrame) -> None:
-        """
-        Create a scatter plot of Rushing Yards vs Fantasy Points.
-        
         Parameters:
-        - df (pd.DataFrame): The DataFrame containing NFL stats.
-        """
-        fig = px.scatter(df, x="RUSHING_YDS", y="MISC_FPTS", color="PLAYER",
-                         title="Rushing Yards vs Fantasy Points",
-                         labels={"RUSHING_YDS": "Rushing Yards", "MISC_FPTS": "Fantasy Points", "PLAYER": "Player"})
-        fig.update_traces(marker=dict(size=8))
-        self.save_plot(fig, "rushing_yards_vs_fantasy_points.html")
+        - player_name (str): The name of the target player.
 
-    def create_deep_balls_vs_fantasy_points(self, df: pd.DataFrame) -> None:
+        Returns:
+        None
         """
-        Create a scatter plot of Deep Balls (20+ Yards) vs Fantasy Points.
-        
-        Parameters:
-        - df (pd.DataFrame): The DataFrame containing NFL stats.
-        """
-        fig = px.scatter(df, x="DEEP_BALL_20+_YDS", y="MISC_FPTS", color="PLAYER", 
-                         title="Deep Balls (20+ Yards) vs Fantasy Points",
-                         labels={"DEEP_BALL_20+_YDS": "Deep Balls 20+ Yards", "MISC_FPTS": "Fantasy Points", "PLAYER": "Player"})
-        fig.update_traces(marker=dict(size=8))
-        self.save_plot(fig, "deep_balls_vs_fantasy_points.html")
+        historical_player_stats = self.get_player_stats(player_name)
+        historical_player_stats["TOTAL_TD"] = (
+            historical_player_stats["PASSING_TD"]
+            + historical_player_stats["RUSHING_TD"]
+        )
+        overall_passing_yards = (
+            historical_player_stats[["PASSING_YDS", "YEAR", "WEEK"]]
+            .groupby(by=["YEAR", "WEEK"])
+            .agg("sum")
+            .reset_index()
+        )
+        overall_rushing_yards = (
+            historical_player_stats[["RUSHING_YDS", "YEAR", "WEEK"]]
+            .groupby(by=["YEAR", "WEEK"])
+            .agg("sum")
+            .reset_index()
+        )
+        overall_touchdowns = (
+            historical_player_stats[["TOTAL_TD", "YEAR", "WEEK"]]
+            .groupby(by=["YEAR", "WEEK"])
+            .agg("sum")
+            .reset_index()
+        )
+        games_missed = (
+            historical_player_stats[historical_player_stats["GAMES"] == 0][["GAMES", "YEAR"]]
+            .groupby(by=["YEAR"])
+            .agg("count")
+            .reset_index()
+        )
 
-    def create_passing_pct_vs_fantasy_points(self, df: pd.DataFrame) -> None:
-        """
-        Create a scatter plot of Passing Completion Percentage vs Fantasy Points.
-        
-        Parameters:
-        - df (pd.DataFrame): The DataFrame containing NFL stats.
-        """
-        fig = px.scatter(df, x="PASSING_PCT", y="MISC_FPTS", color="PLAYER", 
-                         title="Passing Completion Percentage vs Fantasy Points",
-                         labels={"PASSING_PCT": "Passing Completion %", "MISC_FPTS": "Fantasy Points", "PLAYER": "Player"})
-        fig.update_traces(marker=dict(size=8))
-        self.save_plot(fig, "passing_pct_vs_fantasy_points.html")
+        plot_definitions = [
+            (
+                historical_player_stats,
+                "hist",
+                "PASSING_YDS",
+                None,
+                f"{player_name} - Distribution of Passing Yards",
+                "passing_yards_distribution.html",
+            ),
+            (
+                historical_player_stats,
+                "hist",
+                "RUSHING_YDS",
+                None,
+                f"{player_name} - Distribution of Rushing Yards",
+                "rushing_yards_distribution.html",
+            ),
+            (
+                historical_player_stats,
+                "hist",
+                "TOTAL_TD",
+                None,
+                f"{player_name} - Distribution of Touchdowns",
+                "touchdowns_distribution.html",
+            ),
+            (
+                overall_passing_yards,
+                "line",
+                "WEEK",
+                "PASSING_YDS",
+                f"{player_name} - Passing Yards per Game",
+                "passing_yards_season.html",
+            ),
+            (
+                overall_rushing_yards,
+                "line",
+                "WEEK",
+                "RUSHING_YDS",
+                f"{player_name} - Rushing Yards Per Game",
+                "rushing_yards_game.html",
+            ),
+            (
+                overall_touchdowns,
+                "stack_bar",
+                "WEEK",
+                "TOTAL_TD",
+                f"{player_name} - Total TDs Per Game",
+                "touchdowns_game.html",
+            ),
+            (
+                games_missed,
+                "bar",
+                "YEAR",
+                "GAMES",
+                f"{player_name} - Games Missed Per Season",
+                "games_missed_season.html",
+            ),
+        ]
+        for df, plot_type, x_col, y_col, title, file_name in plot_definitions:
+            fig = self.create_plot(df, plot_type, x_col, y_col, title)
+            self.save_plot(fig, file_name)
+        processor_logger.info("EDA completed and plots saved.")
 
 
 def trigger_process() -> None:
     """
-    Trigger web crawler
+    Trigger the data loading and visualization process.
     """
     process = NFLStatsProcessor()
-    historical_data = process.read_data()
-    processor_logger.info(f"Data loaded with {len(historical_data)} rows.")
-    process.create_visualizations(historical_data)
+    historical_stats = process.stats
+    processor_logger.info(f"Data loaded with {len(historical_stats)} rows.")
+    process.generate_and_save_plots("Lamar Jackson")
 
 
 if __name__ == "__main__":
